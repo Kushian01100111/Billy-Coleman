7a2d4bc8b4cd851810a641c92edc3a4352531107
Kushian01100111 <comaspedro6@gmail.com>
2023-03-03 23:35:44 -0300

    03/03/2023

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,12 @@ function App() {
     animate={{opacity: 1}}
     transition={{duration: 0.75, ease:"easeOut"}}>
       <iframe
-      src="https://player.cloudinary.com/embed/?public_id=https%3A%2F%2Fres.cloudinary.com%2Fdpsobnvtv%2Fvideo%2Fupload%2Fv1677783764%2F15_Imagenes_rapido_Advertising_rxzbad.mp4&cloud_name=15_Imagenes_rapido_Advertising_rxzb&player[controls]=false&player[muted]=true&player[hideContextMenu]=true&player[autoplay]=true&player[loop]=true"
+      src="https://player.cloudinary.com/embed/?public_id=https%3A%2F%2Fres.cloudinary.com%2Fdpsobnvtv%2Fvideo%2Fupload%2Fv1677785697%2F15_mas_lento_limksm.mp4&cloud_name=15_mas_lento_limksm&player[controls]=false&player[muted]=true&player[hideContextMenu]=true&player[autoplay]=true&player[loop]=true"
       title="ShowCase"
       allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
       allowfullscreen
       frameborder="0"
+      
     ></iframe>
     </m.div>
   );
