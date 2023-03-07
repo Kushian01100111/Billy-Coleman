6f1afbf929f27d2841209222a13555ae688d6adf
Kushian01100111 <comaspedro6@gmail.com>
2023-03-07 15:41:23 -0300

    07/03/2023

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react"
+import {motion as m } from "framer-motion"
 import "./Slider.css"
 
 
 
 const Slider = ({slides}) => {
   const [currentIndex, setCurrentIndex] =  useState(0);
+  const [goRight, setGoRight] = useState(false)
+  const [goLeft, setGoLeft] = useState(false)
 
     const goToPrevious = () =>{
       const isFirstSlide = currentIndex === 0;
       const newIndex = isFirstSlide ? slides.length -1 : currentIndex - 1;
       setCurrentIndex(newIndex)
+      setGoLeft(true)
+      setGoRight(false)
     };
 
 
@@ -17,19 +22,37 @@ const Slider = ({slides}) => {
       const isLastSlide = currentIndex === slides.length - 1;
       const newIndex =  isLastSlide ? 0: currentIndex + 1;
         setCurrentIndex(newIndex);
+        setGoLeft(false)
+        setGoRight(true)
     };
 
+    console.log(goLeft, goRight)
 
 
   return (
       <div className="img" >
       <div className="left" onClick={()=> goToPrevious()}></div>
         <div className="right" onClick={()=> goToNext()}></div>
-          <img 
+        <m.img  src={`${currentIndex === 0 ? slides[slides.length - 1].url : slides[currentIndex-1].url}`}
+                alt={`${currentIndex === 0 ? slides[slides.length - 1].alt : slides[currentIndex-1].alt}`}
+                  initial={{opacity: 0, display: "none"}}
+                  className="slide"
+                />
+          <m.img key={currentIndex}
             src={`${slides[currentIndex].url}`}
             alt={`${slides[currentIndex].alt}`} 
+            initial={goLeft === goRight ? {opacity:0, x:100} 
+                    : goLeft ? {opacity:0, x:-150}
+                    : goRight ? {opacity:0, x: 150} 
+                    : ""}
+            animate={{opacity:1, x:0}}
             className="slide" 
              />
+        <m.img  src={`${currentIndex === slides.length - 1? slides[0].url : slides[currentIndex+1].url }`}
+                alt={`${currentIndex === slides.length - 1? slides[0].alt : slides[currentIndex+1].alt }`}
+                initial={{opacity: 0, display: "none"}}
+                  className="slide"
+                />
       </div>
   )
 }
--- frontend/src/pages/Women.jsx
+++ frontend/src/pages/Women.jsx
@@ -14,7 +14,7 @@ const Women = () => {
     animate={{opacity: 1}}
     transition={{duration: 0.75, ease:"easeOut"}}>
      <h2 onClick={handleClick}>Portfolio</h2>
-     <div className="iframeContainer" onClick={handleClick}>
+     <div className="iframeContainer" onClick={handleClick} on>
       <iframe 
             src="https://player.cloudinary.com/embed/?public_id=3075397806_t3w9ik&cloud_name=dpsobnvtv&player[fluid]=true&player[controls]=false&player[muted]=true&player[skin]=light&player[autoplay]=true&player[loop]=true"
             allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
