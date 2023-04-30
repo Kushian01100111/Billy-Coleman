c8d98ec7eb2cb81188fcb6f1a668cadd704741db
Kushian01100111 <comaspedro6@gmail.com>
2023-04-29 22:27:10 -0300

    29/04

--- frontend/src/pages/Women.jsx
+++ frontend/src/pages/Women.jsx
@@ -37,7 +37,7 @@ const Women = () => {
      onMouseOver={handleMouseOver}
      onMouseOut={handleMouseOver}>
       <iframe 
-            src="https://player.cloudinary.com/embed/?public_id=https%3A%2F%2Fres.cloudinary.com%2Fdpsobnvtv%2Fvideo%2Fupload%2Fv1681402093%2FWomen%2FWomen_1_stpi1j.mp4&cloud_name=Women_1_stpi1j&player[controls]=false&player[muted]=true&player[hideContextMenu]=false&player[autoplay]=true&player[loop]=true"
+            src="https://player.cloudinary.com/embed/?public_id=https%3A%2F%2Fres.cloudinary.com%2Fdpsobnvtv%2Fvideo%2Fupload%2Fv1681698133%2FWomen%2FWomen_y4c6ud.mp4&cloud_name=Women_y4c6ud&player[controls]=false&player[muted]=true&player[hideContextMenu]=true&player[autoplay]=true&player[loop]=true"
             allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
             allowfullscreen
             frameborder="0"
